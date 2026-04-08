4226e07ea6a22200339802e9e6946938b4c702f5
Pavan <pavankalyan.k2023a@vitstudent.ac.in>
2026-04-08 11:49:17 +0530

    save

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.assertEquals; 
+import org.junit.jupiter.api.Test; 
+public class AppTest { 
+@Test 
+void testAddition() { 
+assertEquals(5, App.add(2, 3)); 
+} 
+}
